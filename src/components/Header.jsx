58caff4bd70cff746488fc5d9c5de7793d4b68e9
Your Name <you@example.com>
2024-09-19 10:26:46 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Icons } from "@/assets/icons";
+
+function Header() {
+  return (
+    <>
+      <div className="header">
+        <div className="header__navbar">
+          <div className="navbar__logo">
+            <Icons icon="datakulture_logo" />
+          </div>
+          <div className="navbar__menu">
+            <span className="navbar__item">Services</span>
+            <span className="navbar__item">Company</span>
+            <span className="navbar__item">Resources</span>
+          </div>
+        </div>
+        <div className="header__button">
+          <button className="header__contact-button">Contact us</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
