8e06a0b587e752e2b833cc53a663f4c524818652
Your Name <you@example.com>
2024-09-29 11:56:08 +0530

    v

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
